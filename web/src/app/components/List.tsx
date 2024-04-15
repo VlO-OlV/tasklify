55dc8fe2776c5e0e1701116686035eaeb8435de5
VlO-OlV <linichenko.vanya@gmail.com>
2024-04-15 23:46:50 +0300

    Added styles for list and task components

--- web/src/app/components/List.tsx
+++ web/src/app/components/List.tsx
@@ -4,8 +4,15 @@ import '../../assets/styles/List.css';
 function List(props: any) {
     return (
         <div className="list">
-            <div></div>
-            <div></div>
+            <div className="list-header">
+                <h3 className="list-title">To Do</h3>
+                <div>
+                    <p>45</p>
+                    <button className="list-menu"></button>
+                </div>
+            </div>
+            <button className="list-add-task">Add new card</button>
+            <Task />
             <Task />
         </div>
     );
--- web/src/app/components/Task.tsx
+++ web/src/app/components/Task.tsx
@@ -3,14 +3,21 @@ import '../../assets/styles/Task.css';
 function Task(props: any) {
     return (
         <div className="task">
-            <div>
-                <h2></h2>
-                
+            <div className="task-header">
+                <h3 className="task-title">Task</h3>
+                <button className="task-menu"></button>
             </div>
-            <p></p>
-            <div></div>
-            <div></div>
-            <div></div>
+            <p className="task-description">Lorem ipsum dolor it amet Lorem ipsum dolor it amet Lorem ipsum dolor it amet</p>
+            <span className="task-deadline">Wed, 19 Apr</span>
+            <span className="task-priority">Low</span>
+            <div className="task-dropdown">
+                <span className="task-dropdown-button">Move to:</span>
+                <div className="task-dropdown-content">
+                    <button className="task-dropdown-content-button">First</button>
+                    <button className="task-dropdown-content-button">Second</button>
+                    <button className="task-dropdown-content-button">Third</button>
+                </div>
+            </div>
         </div>
     );
 }

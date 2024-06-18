ebe3210cf9e133fe7abbe0bddfb8f8737621d863
VlO-OlV <linichenko.vanya@gmail.com>
2024-06-18 23:55:09 +0300

    Completed draft variant of frontend part

--- web/src/app/components/Task.tsx
+++ web/src/app/components/Task.tsx
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import '../../assets/styles/Task.css';
+import Menu from './Menu';
 
 function Task(props: any) {
+    const [isVisibleDropdown, setIsVisibleDropdown] = useState(false);
+    const [isVisibleOptions, setIsVisibleOptions] = useState(false);
+
+    function closeMenu() {
+        setIsVisibleOptions(false);
+    }
+
     return (
-        <div className="task">
+        <div className="task" onClick={() => {props.openModal(1);}}>
             <div className="task-header">
-                <h3 className="task-title">Task</h3>
-                <button className="task-menu"></button>
+                <h3 className="task-title">{props.name}</h3>
+                <button className="task-menu" onClick={(e) => {e.stopPropagation(); setIsVisibleOptions(!isVisibleOptions)}}></button>
             </div>
+            <Menu isList={false} isOpened={isVisibleOptions} closeMenu={closeMenu} openModal={props.openModal}/>
             <p className="task-description">Lorem ipsum dolor it amet Lorem ipsum dolor it amet Lorem ipsum dolor it amet</p>
-            <span className="task-deadline">Wed, 19 Apr</span>
-            <span className="task-priority">Low</span>
-            <div className="task-dropdown">
-                <span className="task-dropdown-button">Move to:</span>
+            <span className="task-deadline">{props.deadline}</span>
+            <span className="task-priority">{props.priority}</span>
+            <div className="task-dropdown" onClick={(e) => {e.stopPropagation();}}>
+                <button className={ isVisibleDropdown ? "task-dropdown-button task-dropdown-button_active" : "task-dropdown-button"} onClick={ () => {setIsVisibleDropdown(!isVisibleDropdown)} }>Move to:</button>
+                { isVisibleDropdown ? 
                 <div className="task-dropdown-content">
                     <button className="task-dropdown-content-button">First</button>
                     <button className="task-dropdown-content-button">Second</button>
-                    <button className="task-dropdown-content-button">Third</button>
-                </div>
+                    <button className="task-dropdown-content-button lastList">Third</button>
+                </div> : null }
             </div>
         </div>
     );

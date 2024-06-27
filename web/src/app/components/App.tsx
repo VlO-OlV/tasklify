20ad7198ee98b1ca27a8b6bd59b6969c804eff07
VlO-OlV <linichenko.vanya@gmail.com>
2024-06-27 17:39:03 +0300

    Added functionality to create and update for tasks and lists

--- web/src/app/components/App.tsx
+++ web/src/app/components/App.tsx
@@ -1,38 +1,21 @@
 import ListBlock from './ListBlock';
 import Nav from './Nav';
 import '../../assets/styles/App.css';
-import Modal from './Modal';
-import React, { useState } from 'react';
+import React from 'react';
 import { useAppDispatch, useAppSelector } from '../hooks/hooks';
-import { List, createList, deleteListById, updateListById } from '../slices/listsSlice';
+import { List, createList } from '../slices/listsSlice';
 
 function App() {
   const lists: List[] = useAppSelector((state) => state.lists.lists);
   const dispatch = useAppDispatch();
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [modalMode, setModalMode] = useState(1);
-
-  function openModal(mode: number) {
-    setModalMode(mode);
-    setIsModalVisible(true);
-  }
-
   function addList() {
     dispatch(createList("New List"));
   }
 
-  function deleteList(listId: string) {
-    dispatch(deleteListById(listId));
-  }
-
-  function updateList(listData: List) {
-    dispatch(updateListById(listData));
-  }
-
   function renderLists(lists: List[]): React.ReactElement[] {
-    const listComponents = lists.map((list) => <ListBlock data={list} openModal={openModal} deleteList={deleteList}/>);
-    return listComponents;
+    const listBlocks = lists.map((list) => <ListBlock data={list}/>);
+    return listBlocks;
   }
 
   return (
@@ -43,7 +26,6 @@ function App() {
           {renderLists(lists)}
         </div>
       </main>
-      { isModalVisible ? <Modal closeModal={() => {setIsModalVisible(false);}} mode={modalMode} changeMode={(mode: number) => {setModalMode(mode);}} /> : null }
     </div>
   );
 }

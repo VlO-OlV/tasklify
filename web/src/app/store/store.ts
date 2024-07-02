eef9eff9cd9e05ac3edf804ab35cbe990b39e5e5
VlO-OlV <linichenko.vanya@gmail.com>
2024-07-02 22:47:50 +0300

    Prepared api slices for tasks and lists

--- web/src/app/store/store.ts
+++ web/src/app/store/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { tasksReducer } from "./slices/tasksSlice";
 import { listsReducer } from "./slices/listsSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
     reducer: {
         tasks: tasksReducer,
         lists: listsReducer,
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(baseApi.middleware);
     },
 });
 
